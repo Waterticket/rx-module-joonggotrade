de4603435c5839b389000a5c5c4f2de92db07be5
Waterticket <matthew218@naver.com>
2021-10-07 14:43:46 +0900

    Trade Status와 Rating 추가

--- joonggotrade.controller.php
+++ joonggotrade.controller.php
@@ -18,7 +18,19 @@ class JoonggotradeController extends Joonggotrade
 	 */
 	public function triggerAfterInsertDocument($obj)
 	{
-	
+		$logged_info = Context::get('logged_info');
+		$price = $obj->extra_vars1;
+		preg_replace("/[^0-9]/", "", $price);
+
+		$args = new stdClass();
+		$args->document_srl = $obj->document_srl;
+		$args->seller_srl = $logged_info->member_srl;
+		$args->buyer_srl = 0;
+		$args->trade_price = $price;
+		$args->trade_status = 200;
+		$args->insert_time = time();
+		$args->update_time = time();
+		executeQuery('joonggotrade.insertTrade', $args);
 	}
 	
 	/**
@@ -40,40 +52,62 @@ class JoonggotradeController extends Joonggotrade
 	 */
 	public function triggerAfterDeleteDocument($obj)
 	{
-	
+		$args = new stdClass();
+		$args->document_srl = $obj->document_srl;
+		executeQuery('joonggotrade.deleteTrade', $args);
 	}
-	
-	/**
-	 * 트리거 예제: 새 댓글 작성시 실행
-	 * 
-	 * 주의: 첨부파일이 있는 경우 아직 작성하지 않았어도 이 함수가 실행될 수 있음
-	 * 
-	 * @param object $obj
-	 */
-	public function triggerAfterInsertComment($obj)
+
+	public function procJoonggotradeChangeStatus()
 	{
-	
+		// 현재 설정 상태 불러오기
+		$config = $this->getConfig();
+		
+		// 제출받은 데이터 불러오기
+		$vars = Context::getRequestVars();
+
+		$code = $vars->code;
+		$document_srl = $vars->document_srl;
+		$buyer_srl = $vars->buyer_srl;
+
+		$args = new stdClass();
+		$args->document_srl = $document_srl;
+		$args->trade_status = $code;
+		$args->update_time = time();
+		switch($code)
+		{
+			case 200:
+				$args->buyer_srl = 0;
+				break;
+
+			case 201:
+				$args->buyer_srl = $buyer_srl;
+				break;
+		}
+
+		$output = executeQuery('joonggotrade.updateTrade', $args);
+		exit;
 	}
-	
-	/**
-	 * 트리거 예제: 댓글 수정시 실행
-	 * 
-	 * 주의: 첨부파일이 있는 경우 최종 작성 시점에 이 함수가 실행될 수 있음
-	 * 
-	 * @param object $obj
-	 */
-	public function triggerAfterUpdateComment($obj)
+
+	public function procJoonggotradePeerEvaluation()
 	{
-	
-	}
-	
-	/**
-	 * 트리거 예제: 댓글 삭제시 실행
-	 * 
-	 * @param object $obj
-	 */
-	public function triggerAfterDeleteComment($obj)
-	{
-	
+		// 현재 설정 상태 불러오기
+		$config = $this->getConfig();
+		
+		// 제출받은 데이터 불러오기
+		$vars = Context::getRequestVars();
+
+		$rating = $vars->rating;
+		$giver = $vars->member_srl;
+		$receiver = $vars->target_srl;
+		$document_srl = $vars->document_srl;
+
+		$args = new stdClass();
+		$args->giver = $giver;
+		$args->receiver = $receiver;
+		$args->star = $rating;
+		$args->document_srl = $document_srl;
+		$args->insert_time = time();
+		$output = executeQuery('joonggotrade.insertRating', $args);
+		exit;
 	}
 }
--- joonggotrade.model.php
+++ joonggotrade.model.php
@@ -9,5 +9,56 @@
  */
 class JoonggotradeModel extends Joonggotrade
 {
-	
+	public static function getTradeStatusHTML($document_srl)
+    {
+        $args = new stdClass();
+        $args->document_srl = $document_srl;
+        $output = executeQuery('joonggotrade.getTradeByDocumentSrl', $args);
+
+        $trade = $output->data;
+        switch($trade->trade_status)
+        {
+            case 200:
+                return "<span style='color: #e67e22'>[판매중]</span>";
+
+            case 201:
+                return "<span style='color: #3498db'>[거래 예정]</span>";
+
+            case 202:
+                return "<span style='color: #95a5a6'>[판매 완료]</span>";
+
+            case 300:
+                return "<span style='color: #95a5a6'>[취소됨]</span>";
+        }
+    }
+
+    public static function getTradeStatus($document_srl)
+    {
+        $args = new stdClass();
+        $args->document_srl = $document_srl;
+        $output = executeQuery('joonggotrade.getTradeByDocumentSrl', $args);
+
+        $trade = $output->data;
+        return $trade->trade_status;
+    }
+
+    public static function getTrade($document_srl)
+    {
+        $args = new stdClass();
+        $args->document_srl = $document_srl;
+        $output = executeQuery('joonggotrade.getTradeByDocumentSrl', $args);
+
+        $trade = $output->data;
+        return $trade;
+    }
+
+    public static function getRating($document_srl, $giver_srl)
+    {
+        $args = new stdClass();
+        $args->document_srl = $document_srl;
+        $args->giver = $giver_srl;
+        $output = executeQuery('joonggotrade.getMyDocumentRating', $args);
+
+        return $output->data;
+    }
 }
